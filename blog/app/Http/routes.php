13345b24f49e4ad84dcace8bb3c9fe62088f26c4
kaihan chang <kaihanhh@gmail.com>
2017-01-16 13:18:57 +0800

    Add Route for testing nested group prefix

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -14,3 +14,27 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+
+$app->group(['prefix' => 'api/v1/'], function () use ($app) {
+
+    $app->group(['prefix' => 'user/', 'namespace' => 'App\Http\Controllers\User'], function () use ($app) {
+        $app->get('foo', function () {
+            return 'Hello foo';
+        });
+        $app->get('bar', function () {
+            return 'Hello bar';
+        });
+    });
+
+    $app->group(['prefix' => 'job/', 'namespace' => 'App\Http\Controllers\Job'], function () use ($app) {
+        $app->get('foo', function () {
+            return 'Hello fooJob';
+        });
+        $app->get('bar', function () {
+            return 'Hello barJob';
+        });
+    });
+    
+
+});
